f82f7aef76a32f8cf580797a4b4c4e033b8aa7ca
Bartosz Ratajczak <ratajczak89@gmail.com>
2015-10-26 14:19:07 +0100

    HandEvaluator refactor

--- src/main/java/com/capgemini/pokerHands/HandEvaluator.java
+++ src/main/java/com/capgemini/pokerHands/HandEvaluator.java
@@ -4,20 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HandEvaluator {
+	Hand hand;
+	String[] cards;
+	int[] values;
+	boolean isHandFlush;
+	boolean isHandStraight;
 
-	public static Hand evaluateHand(String rawHand) {
-		Hand hand = new Hand();
-		String[] cards = rawHand.split(" ");
-		
-		// obtaining cards
+	public Hand evaluateHand(String rawHand) {
+		hand = new Hand();
+		cards = rawHand.split(" ");
+		values = new int[cards.length];
+
+		obtainCards();
+		countCardsOccurence();
+		checkCardsColors();
+		checkCardsInterval();
+
+		checkPairs(hand);
+		checkTwoPairs(hand);
+		checkSet(hand);
+		checkStraight(hand, isHandStraight);
+		checkFlush(hand, isHandFlush);
+		checkFullHouse(hand);
+		checkFourOfAKind(hand);
+
+		return hand;
+	}
+	
+	private void obtainCards() {
 		String cardName;
-		int[] values = new int[cards.length];
 		for (int i = 0; i < cards.length; i++) {
 			cardName = cards[i].substring(0, 1);
 			values[i] = CardValue.getKeyOf(cardName);
 		}
-		
-		// setting cards occurence
+	}
+
+	private void countCardsOccurence() {
 		int key;
 		for (int i = 0; i < values.length; i++) {
 			key = values[i];
@@ -27,80 +49,72 @@ public class HandEvaluator {
 				hand.putCard(key, 1);
 			}
 		}
-			
-		// looking for flush
+	}
+
+	private void checkCardsColors() {
 		String color1, color2;
-		boolean isHandFlush = true;
-		for (int i=1; i<cards.length; i++) {
-			color1 = cards[i-1].substring(1, 2);
+		isHandFlush = true;
+		for (int i = 1; i < cards.length; i++) {
+			color1 = cards[i - 1].substring(1, 2);
 			color2 = cards[i].substring(1, 2);
 			if (!color1.equals(color2)) {
 				isHandFlush = false;
 			}
 		}
-		
-		// looking for straight
+	}
+
+	private void checkCardsInterval() {
 		int value1, value2;
-		boolean isHandStraight = true;
+		isHandStraight = true;
 		List<Integer> valuesList = new ArrayList<Integer>(hand.getCardDistribution().keySet());
-		for (int i=1; i<valuesList.size(); i++) {
-			value1 = valuesList.get(i-1);
+		for (int i = 1; i < valuesList.size(); i++) {
+			value1 = valuesList.get(i - 1);
 			value2 = valuesList.get(i);
 			if ((value2 - value1 != 1) && (value2 - value1 != 9)) {
 				isHandStraight = false;
 			}
 		}
-
-		checkPairs(hand);
-		checkTwoPairs(hand);
-		checkSet(hand);
-		checkStraight(hand, isHandStraight);
-		checkFlush(hand, isHandFlush);
-		checkFullHouse(hand);
-		checkFourOfAKind(hand);
-		
-		return hand;
 	}
 
-	private static void checkFourOfAKind(Hand hand) {
-		if (hand.getCardDistribution().containsValue(4)) {
-			hand.setHandStrength(7);
+	private void checkPairs(Hand hand) {
+		if (hand.getCardDistribution().containsValue(2) && hand.getCardDistribution().size() == 4) {
+			hand.setHandStrength(1);
 		}
 	}
-
-	private static void checkFullHouse(Hand hand) {
+	
+	private void checkTwoPairs(Hand hand) {
+		if (hand.getCardDistribution().containsValue(2) && hand.getCardDistribution().size() == 3) {
+			hand.setHandStrength(2);
+		}
+	}
+	
+	private void checkSet(Hand hand) {
+		if (hand.getCardDistribution().containsValue(3) && hand.getCardDistribution().size() == 3) {
+			hand.setHandStrength(3);
+		}
+	}
+	
+	private void checkStraight(Hand hand, boolean isHandStraight) {
+		if (isHandStraight) {
+			hand.setHandStrength(hand.getHandStrength() + 4);
+		}
+	}
+	
+	private void checkFlush(Hand hand, boolean isHandFlush) {
+		if (isHandFlush) {
+			hand.setHandStrength(hand.getHandStrength() + 5);
+		}
+	}
+	
+	private void checkFullHouse(Hand hand) {
 		if (hand.getCardDistribution().containsValue(3) && hand.getCardDistribution().containsValue(2)) {
 			hand.setHandStrength(6);
 		}
 	}
 
-	private static void checkFlush(Hand hand, boolean isHandFlush) {
-		if (isHandFlush) {
-			hand.setHandStrength(hand.getHandStrength()+5);
-		}
-	}
-
-	private static void checkStraight(Hand hand, boolean isHandStraight) {
-		if (isHandStraight) {
-			hand.setHandStrength(hand.getHandStrength()+4);
-		}
-	}
-
-	private static void checkSet(Hand hand) {
-		if (hand.getCardDistribution().containsValue(3) && hand.getCardDistribution().size() == 3) {
-			hand.setHandStrength(3);
-		}
-	}
-
-	private static void checkTwoPairs(Hand hand) {
-		if (hand.getCardDistribution().containsValue(2) && hand.getCardDistribution().size() == 3) {
-			hand.setHandStrength(2);
-		}
-	}
-
-	private static void checkPairs(Hand hand) {
-		if (hand.getCardDistribution().containsValue(2) && hand.getCardDistribution().size() == 4) {
-			hand.setHandStrength(1);
+	private void checkFourOfAKind(Hand hand) {
+		if (hand.getCardDistribution().containsValue(4)) {
+			hand.setHandStrength(7);
 		}
 	}
 }
--- src/test/java/com/capgemini/pokerHands/HandEvaluatorTest.java
+++ src/test/java/com/capgemini/pokerHands/HandEvaluatorTest.java
@@ -2,57 +2,64 @@ package com.capgemini.pokerHands;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class HandEvaluatorTest {
+	HandEvaluator evaluator;
 
+	@Before
+	public void executeBeforeEachEvaluation() {
+		evaluator = new HandEvaluator();
+	}
+	
 	@Test
 	public void shouldDetectOnePair() {
 		String rawHand = "AC AD 9H QS TC";
-		assertThat(HandEvaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(1);
+		assertThat(evaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(1);
 	}
 
 	@Test
 	public void shouldDetectTwoPairs() {
 		String rawHand = "AC AD 9H 9S TC";
-		assertThat(HandEvaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(2);
+		assertThat(evaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(2);
 	}
 
 	@Test
 	public void shouldDetectThreeOfAKind() {
 		String rawHand = "AC AD AH 9S TC";
-		assertThat(HandEvaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(3);
+		assertThat(evaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(3);
 	}
 
 	@Test
 	public void shouldDetectFullHouse() {
 		String rawHand = "AC AD AH 9S 9C";
-		assertThat(HandEvaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(6);
+		assertThat(evaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(6);
 	}
 
 	@Test
 	public void shouldDetectFourOfAKind() {
 		String rawHand = "AC AD AH AS 9C";
-		assertThat(HandEvaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(7);
+		assertThat(evaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(7);
 	}
 	
 	@Test
 	public void shouldDetectFlush() {
 		String rawHand = "AC KC 4C 6C 9C";
-		assertThat(HandEvaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(5);
+		assertThat(evaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(5);
 	}
 	
 	@Test
 	public void shouldDetectStraight() {
 		String rawHand = "2D AC 3C 4S 5C";
-		assertThat(HandEvaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(4);
+		assertThat(evaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(4);
 	}
 	
 	@Test
 	public void shouldDetectHighCard() {
 		String rawHand = "6D KC 5C TS 9C";
-		assertThat(HandEvaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(0);
+		assertThat(evaluator.evaluateHand(rawHand).getHandStrength()).isEqualTo(0);
 	}
 
 }
